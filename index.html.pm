241f34d0aad168fa4a8ad0a6e6560c27444e36ed
Roman Kashitsyn <roman@dfinity.org>
2021-10-24 00:08:40 +0200

    Show last post on the index page

--- index.html.pm
+++ index.html.pm
@@ -1,4 +1,14 @@
 #lang pollen
 
+◊(require racket/list
+          racket/string
+          pollen/pagetree)
+
 ◊(define-meta title "mmap(blog)")
-◊(define-meta author "Roman Kashitsyn")
+
+◊(define all-posts (children 'posts.html (get-pagetree "index.ptree")))
+◊(define last-post (last all-posts))
+◊(define last-post-metas (get-metas last-post))
+
+◊h1{◊(select-from-metas 'title last-post-metas)}
+◊@{◊(get-doc last-post)}
