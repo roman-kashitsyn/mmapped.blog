ea113021b8751ceb4147d1ec1b89db2f68acbf8f
Roman Kashitsyn <roman@dfinity.org>
2021-11-30 16:26:54 +0100

    Add basic post navigation

--- index.html.pm
+++ index.html.pm
@@ -6,9 +6,17 @@
 
 ◊(define-meta title "mmap(blog)")
 
-◊(define all-posts (children 'posts.html (get-pagetree "index.ptree")))
-◊(define last-post (last all-posts))
+◊(define page-tree (get-pagetree "index.ptree"))
+◊(define all-posts (children 'posts.html page-tree))
+◊(define last-post (first all-posts))
 ◊(define last-post-metas (get-metas last-post))
+◊(define prev-page (next last-post page-tree))
 
 ◊h1{◊a[#:href ◊(symbol->string last-post)]{◊(select-from-metas 'title last-post-metas)}}
 ◊@{◊(get-doc last-post)}
+
+◊when/splice[(and prev-page (member prev-page all-posts))]{
+  ◊div[#:id "next-prev-nav"]{
+    ◊div[#:id "older"]{◊a[#:href (symbol->string prev-page)]{◊(select-from-metas 'title prev-page)→ }}
+  }
+}
